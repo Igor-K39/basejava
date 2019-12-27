3a89016f75d7de01b56f6f69119c66f8a1584350
Igor-K39 <kopyshev.sd@gmail.com>
2019-12-27 08:51:06 +0300

    HW_6: AbstractStorageTest.java - fixed test getAllSorted()

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -44,7 +44,7 @@ public abstract class AbstractStorageTest {
 
     @Test
     public void getAllSorted() {
-        Assert.assertEquals(RESUME_LIST.size(), storage.size());
+        Assert.assertEquals(RESUME_LIST.size(), storage.getAllSorted().size());
         Assert.assertEquals(RESUME_LIST, storage.getAllSorted());
     }
 
