ee5efedc89d4e295afbc40ccd1f146c1bf8c9d7b
Igor-K39 <kopyshev.sd@gmail.com>
2020-01-05 23:22:12 +0300

    HW_7 | Implemented Section.java and TextSection.java

---
+++ src/com/urise/webapp/model/TextSection.java
@@ -0,0 +1,22 @@
+package com.urise.webapp.model;
+
+public class TextSection extends Section {
+    private String text;
+
+    public TextSection(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+}
