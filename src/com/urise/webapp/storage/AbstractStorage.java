ea67b5f1025093084e86746b9d926422558110dd
Igor-K39 <kopyshev.sd@gmail.com>
2020-01-05 23:14:25 +0300

    Implemented logger in AbstractStorage.java

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -6,17 +6,21 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
-    protected final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid);
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+    private final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid);
 
     @Override
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK searchKey = getCheckedKey(uuid, true);
         return getResume(searchKey);
     }
 
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> resumes = getAllResumes();
         resumes.sort(RESUME_COMPARATOR);
         return resumes;
@@ -24,18 +28,21 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public void save(Resume resume) {
+        LOG.info("Save " + resume);
         SK searchKey = getCheckedKey(resume.getUuid(), false);
         saveResume(searchKey, resume);
     }
 
     @Override
     public void update(Resume resume) {
+        LOG.info("Update " + resume);
         SK searchKey = getCheckedKey(resume.getUuid(), true);
         updateResume(searchKey, resume);
     }
 
     @Override
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK searchKey = getCheckedKey(uuid, true);
         deleteResume(searchKey);
     }
@@ -44,10 +51,12 @@ public abstract class AbstractStorage<SK> implements Storage {
         SK searchKey = getSearchKey(uuid);
         if (existence) {
             if (!isExist(searchKey)) {
+                LOG.warning("Resume " + uuid + " not exists");
                 throw new NotExistStorageException(uuid);
             }
         } else {
             if (isExist(searchKey)) {
+                LOG.warning("Resume " + uuid + " already exists");
                 throw new ExistStorageException(uuid);
             }
         }
