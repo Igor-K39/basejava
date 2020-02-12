1a9be05a6a66cf8d2ebcea71936d86a74b0f2954
Igor Kopyshev <kopyshev.sd@gmail.com>
2020-02-12 11:46:25 +0300

    HW_7 | ListSection.java - overrided equals() and hashcode()

--- src/com/urise/webapp/model/ListSection.java
+++ src/com/urise/webapp/model/ListSection.java
@@ -15,6 +15,20 @@ public class ListSection extends Section {
         return strings;
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListSection that = (ListSection) o;
+        return Objects.equals(strings, that.strings);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(strings);
+    }
+
     @Override
     public String toString() {
         return strings.toString();
