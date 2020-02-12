29f9564056e0c93d741a5ab5f452a9fe8db0998e
Igor Kopyshev <kopyshev.sd@gmail.com>
2020-02-12 11:49:20 +0300

    HW_7 | TextSection.java - text cannot be null

--- src/com/urise/webapp/model/TextSection.java
+++ src/com/urise/webapp/model/TextSection.java
@@ -1,18 +1,17 @@
 package com.urise.webapp.model;
 
+import java.util.Objects;
+
 public class TextSection extends Section {
-    private String text;
+    private final String text;
 
     public TextSection(String text) {
+        Objects.requireNonNull(text, "Text must not be null");
         this.text = text;
     }
 
     public String getText() {
         return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
     }
 
     @Override
