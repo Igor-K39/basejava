b53582c047cb7f7580b92271e8a006635811020c
Igor Kopyshev <kopyshev.sd@gmail.com>
2020-02-12 11:48:09 +0300

    HW_7 | Refactored Period.java

--- src/com/urise/webapp/model/Period.java
+++ src/com/urise/webapp/model/Period.java
@@ -4,15 +4,15 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class Period {
-    private String title;
-    private String description;
-    private LocalDate startDate;
-    private LocalDate endDate;
+    private final String title;
+    private final String description;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
 
     public Period(String title, String description, LocalDate startDate, LocalDate endDate) {
         Objects.requireNonNull(title, "title must not be null");
-        Objects.requireNonNull(description, "description must nut be null");
         Objects.requireNonNull(startDate, "startDate must not be null");
+        Objects.requireNonNull(endDate, "endDate must not be null");
         this.title = title;
         this.description = description;
         this.startDate = startDate;
@@ -23,32 +23,32 @@ public class Period {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public LocalDate getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
     public LocalDate getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Period period = (Period) o;
+        return Objects.equals(title, period.title) &&
+                Objects.equals(description, period.description) &&
+                Objects.equals(startDate, period.startDate) &&
+                Objects.equals(endDate, period.endDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description, startDate, endDate);
     }
 
     @Override
