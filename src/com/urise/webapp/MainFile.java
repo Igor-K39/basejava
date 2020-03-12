a056fcfa90948d9184db0847b73f1bb6656b6a6c
Igor Kopyshev <kopyshev.sd@gmail.com>
2020-03-12 17:50:47 +0300

    HW_8 | MainFile.java - implemented printContent() method

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -3,9 +3,10 @@ package com.urise.webapp;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String filePath = ".\\.gitignore";
 
         File file = new File(filePath);
@@ -28,6 +29,19 @@ public class MainFile {
             System.out.println(fileInputStream.read());
         } catch (IOException e) {
             throw new RuntimeException("Error", e);
+        }
+
+        System.out.println("\nHW_8 starts here");
+        String project_dir = System.getProperty("user.dir");
+        printContent(new File(project_dir));
+    }
+
+    private static void printContent(File directory) throws IOException {
+        for (File file : Objects.requireNonNull(directory.listFiles())) {
+            System.out.println(file.getCanonicalPath());
+            if (file.isDirectory()) {
+                printContent(file);
+            }
         }
     }
 }
