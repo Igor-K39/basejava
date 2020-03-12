6e119dc30e2cf576dcfe5ac6c1f9e25e387daf47
Igor Kopyshev <kopyshev.sd@gmail.com>
2020-03-12 16:41:15 +0300

    The files start with Main* has been moved to webapp package

--- src/com/urise/webapp/MainDate.java
+++ src/com/urise/webapp/MainDate.java
@@ -1,3 +1,5 @@
+package com.urise.webapp;
+
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -1,12 +1,20 @@
 package com.urise.webapp;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class MainFile {
-    public static void main(String[] args) throws IOException {
-        File file = new File(".\\.gitignore");
-        System.out.println(file.getCanonicalPath());
+    public static void main(String[] args) {
+        String filePath = ".\\.gitignore";
+
+        File file = new File(filePath);
+        try {
+            System.out.println(file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
+
         File dir = new File("./src/com/urise/webapp");
         System.out.println(dir.isDirectory());
         String[] list = dir.list();
@@ -14,6 +22,12 @@ public class MainFile {
             for (String name : list) {
                 System.out.println(name);
             }
+        }
+
+        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+            System.out.println(fileInputStream.read());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
         }
     }
 }
