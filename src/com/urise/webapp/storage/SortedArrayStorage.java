220e02e968e78ddbe9d6f6da76344919920cc0aa
Igor Kopyshev <kopyshev.sd@gmail.com>
2020-08-30 18:04:38 +0300

    HW_8 | SortedArrayStorage - refactoring

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -13,7 +13,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void doSaveResume(Integer index, Resume resume) {
-        index = (index + 1) * (-1);
+        index = -index - 1;
         System.arraycopy(storage, index, storage, index + 1, size - index);
         storage[index] = resume;
     }
