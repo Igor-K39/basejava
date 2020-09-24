a3342abb7c4cb86e1342eb7b7457f2daf3887d60
Igor Kopyshev <kopyshev.sd@gmail.com>
2020-09-24 09:33:36 +0300

    HW_10: DataStreamSerializer - refactoring

--- src/com/urise/webapp/storage/serializers/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializers/DataStreamSerializer.java
@@ -1,7 +1,6 @@
 package com.urise.webapp.storage.serializers;
 
 import com.urise.webapp.model.*;
-import com.urise.webapp.util.LocalDateAdapter;
 
 import java.io.*;
 import java.time.LocalDate;
@@ -11,8 +10,6 @@ import java.util.List;
 import java.util.Map;
 
 public class DataStreamSerializer implements StreamSerializer {
-    private final LocalDateAdapter ADAPTER = new LocalDateAdapter();
-
     @Override
     public void doWrite(Resume resume, OutputStream os) throws IOException {
         try (DataOutputStream dos = new DataOutputStream(os)) {
@@ -65,72 +62,90 @@ public class DataStreamSerializer implements StreamSerializer {
 
     private void writeSection(DataOutputStream dos, SectionType type, AbstractSection section) throws IOException {
         dos.writeUTF(type.name());
-        if (type == SectionType.OBJECTIVE || type == SectionType.PERSONAL) {
-            String text = ((TextSection) section).getText();
-            dos.writeUTF(text);
-        } else if (type == SectionType.ACHIEVEMENT || type == SectionType.QUALIFICATIONS) {
-            List<String> strings = ((ListSection) section).getStrings();
-            writeCollection(dos, strings, dos::writeUTF);
-        } else if (type == SectionType.EXPERIENCE || type == SectionType.EDUCATION) {
-            List<Organization> organizations = ((OrganizationSection) section).getOrganizations();
-            writeCollection(dos, organizations, organization -> {
-                dos.writeUTF(organization.getName());
-                dos.writeUTF(organization.getWebsite());
-
-                List<Period> periods = organization.getPeriods();
-                writeCollection(dos, periods, period -> {
-                    dos.writeUTF(period.getTitle());
-                    dos.writeUTF(period.getDescription());
-                    dos.writeUTF(ADAPTER.marshal(period.getStartDate()));
-                    dos.writeUTF(ADAPTER.marshal(period.getEndDate()));
-                });
-            });
+        switch (type) {
+            case OBJECTIVE:
+            case PERSONAL:
+                String text = ((TextSection) section).getText();
+                dos.writeUTF(text);
+                break;
+            case ACHIEVEMENT:
+            case QUALIFICATIONS:
+                List<String> strings = ((ListSection) section).getStrings();
+                writeCollection(dos, strings, dos::writeUTF);
+                break;
+            case EXPERIENCE:
+            case EDUCATION:
+                writeOrganizationSection(dos, (OrganizationSection) section);
+                break;
         }
     }
 
-    private interface Reader<T> {
+    private void writeOrganizationSection(DataOutputStream dos, OrganizationSection section) throws IOException {
+        List<Organization> organizations = section.getOrganizations();
+        writeCollection(dos, organizations, organization -> {
+            dos.writeUTF(organization.getName());
+            dos.writeUTF(organization.getWebsite());
+
+            List<Period> periods = organization.getPeriods();
+            writeCollection(dos, periods, period -> {
+                dos.writeUTF(period.getTitle());
+                dos.writeUTF(period.getDescription());
+                dos.writeUTF(period.getStartDate().toString());
+                dos.writeUTF(period.getEndDate().toString());
+            });
+        });
+    }
+
+    private interface Reader {
         void read() throws IOException;
     }
 
-    private <T> void readElements(DataInputStream dis, Reader<T> reader) throws IOException {
+    private void readElements(DataInputStream dis, Reader reader) throws IOException {
         int size = dis.readInt();
         for (int i = 0; i < size; i++) {
             reader.read();
         }
     }
 
-    private <T> AbstractSection readSection(DataInputStream dis, SectionType type) throws IOException {
+    private AbstractSection readSection(DataInputStream dis, SectionType type) throws IOException {
         AbstractSection section;
-
-        if (type == SectionType.OBJECTIVE || type == SectionType.PERSONAL) {
-            section = new TextSection(dis.readUTF());
-        } else if (type == SectionType.ACHIEVEMENT || type == SectionType.QUALIFICATIONS) {
-            List<String> strings = new ArrayList<>();
-            readElements(dis, () -> strings.add(dis.readUTF()));
-            section = new ListSection(strings);
-        } else if (type == SectionType.EXPERIENCE || type == SectionType.EDUCATION) {
-            List<Organization> organizations = new ArrayList<>();
-            readElements(dis, () -> {
-                String name = dis.readUTF();
-                String website = dis.readUTF();
-                List<Period> periods = new ArrayList<>();
-
-                readElements(dis, () -> {
-                    String title = dis.readUTF();
-                    String description = dis.readUTF();
-                    LocalDate startDate = ADAPTER.unmarshal(dis.readUTF());
-                    LocalDate endDate = ADAPTER.unmarshal(dis.readUTF());
-                    periods.add(new Period(title, description, startDate, endDate));
-                });
-                organizations.add(new Organization(name, website, periods));
-            });
-
-            section = new OrganizationSection(organizations);
-        } else {
-            throw new IllegalStateException("Wrong section type: " + type.name());
+        switch (type) {
+            case OBJECTIVE:
+            case PERSONAL:
+                section = new TextSection(dis.readUTF());
+                break;
+            case ACHIEVEMENT:
+            case QUALIFICATIONS:
+                List<String> strings = new ArrayList<>();
+                readElements(dis, () -> strings.add(dis.readUTF()));
+                section = new ListSection(strings);
+                break;
+            case EXPERIENCE:
+            case EDUCATION:
+                section = readOrganizationSection(dis);
+                break;
+            default:
+                throw new IllegalStateException("Wrong section type: " + type.name());
         }
         return section;
     }
+
+    private OrganizationSection readOrganizationSection(DataInputStream dis) throws IOException {
+        List<Organization> organizations = new ArrayList<>();
+        readElements(dis, () -> {
+            String name = dis.readUTF();
+            String website = dis.readUTF();
+            List<Period> periods = new ArrayList<>();
+
+            readElements(dis, () -> {
+                String title = dis.readUTF();
+                String description = dis.readUTF();
+                LocalDate startDate = LocalDate.parse(dis.readUTF());
+                LocalDate endDate = LocalDate.parse(dis.readUTF());
+                periods.add(new Period(title, description, startDate, endDate));
+            });
+            organizations.add(new Organization(name, website, periods));
+        });
+        return new OrganizationSection(organizations);
+    }
 }
-
-
