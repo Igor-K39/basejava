4da8c19934192c83557795baa72866703b5a0844
Igor Kopyshev <kopyshev.sd@gmail.com>
2020-10-01 21:46:01 +0300

    Refactoring in StorageException

--- src/com/urise/webapp/exception/StorageException.java
+++ src/com/urise/webapp/exception/StorageException.java
@@ -2,9 +2,9 @@ package com.urise.webapp.exception;
 
 public class StorageException extends RuntimeException {
     private final String uuid;
+
     public StorageException(String message) {
-        super(message);
-        this.uuid = null;
+        this(message, null, null);
     }
 
     public StorageException(String message, String uuid) {
@@ -12,9 +12,12 @@ public class StorageException extends RuntimeException {
         this.uuid = uuid;
     }
 
+    public StorageException(Exception e) {
+        this(e.getMessage(), e);
+    }
+
     public StorageException(String message, Exception cause) {
-        super(message, cause);
-        this.uuid = null;
+        this(message, null, cause);
     }
 
     public StorageException(String message, String uuid, Exception cause) {
