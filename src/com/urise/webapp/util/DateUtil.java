bf98f7acfdb5ab7e02e7229577385ecd631c6048
Igor Kopyshev <kopyshev.sd@gmail.com>
2020-11-05 23:07:00 +0300

    HW_16: Updated DateUtil.java

--- src/com/urise/webapp/util/DateUtil.java
+++ src/com/urise/webapp/util/DateUtil.java
@@ -1,10 +1,18 @@
 package com.urise.webapp.util;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
     public static LocalDate of(int year, Month month) {
         return LocalDate.of(year, month, 1);
+    }
+
+    public static String getFormattedAsPeriod(LocalDate startDate, LocalDate endDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/yyyy");
+        return startDate.format(formatter) + " - " + endDate.format(formatter);
     }
 }
